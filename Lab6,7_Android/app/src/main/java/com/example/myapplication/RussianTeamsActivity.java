a90791ac808d20fe94ac13cbc4bb94ff04b50df0
Michał Ryński <michalrynski96@gmail.com>
2020-06-23 14:28:58 +0200

    lab 4,6,7 uploaded again

---
+++ Lab6,7_Android/app/src/main/java/com/example/myapplication/RussianTeamsActivity.java
@@ -0,0 +1,65 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class RussianTeamsActivity extends AppCompatActivity {
+    private SQLiteDatabase db;
+    private Cursor cursor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_russian_teams);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ListView listView = (ListView) findViewById(R.id.list_russian);
+        SQLiteOpenHelper dbHelper = new DatabaseHelper(this);
+        try {
+            db = dbHelper.getReadableDatabase();
+            cursor = db.query("RU_TEAM", new String[] {"_id", "NAME"},
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[] {"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            listView.setAdapter(listAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "DB nie jest dostepna", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(RussianTeamsActivity.this, RuTeamActivity.class);
+                intent.putExtra(RuTeamActivity.RUTEAM_ID, (int) id);
+                startActivity(intent);
+            }
+        };
+        listView.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
+}
